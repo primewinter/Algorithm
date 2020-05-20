2f96be8f7c31ce7d9a718d1cbbbd2119c2c55eb0
ChaSoyoung <yuuni.cha@gmail.com>
2020-05-20 22:09:54 +0900

    20-05-20 Update StockPrice
    add other's solution

--- Algorithm/src/programmers/level02/StockPrice.java
+++ Algorithm/src/programmers/level02/StockPrice.java
@@ -3,18 +3,37 @@ package programmers.level02;
 import java.util.ArrayList;
 
 public class StockPrice {
+	/* 주식가격 +7
+	 * https://programmers.co.kr/learn/courses/30/lessons/42584
+	 * 문제 설명
+	초 단위로 기록된 주식가격이 담긴 배열 prices가 매개변수로 주어질 때, 가격이 떨어지지 않은 기간은 몇 초인지를 return 하도록 solution 함수를 완성하세요.
+	
+	제한사항
+	prices의 각 가격은 1 이상 10,000 이하인 자연수입니다.
+	prices의 길이는 2 이상 100,000 이하입니다.
+	입출력 예
+	prices	return
+	[1, 2, 3, 2, 3]	[4, 3, 1, 1, 0]
+	입출력 예 설명
+	1초 시점의 1은 끝까지 가격이 떨어지지 않았습니다.
+	2초 시점의 2은 끝까지 가격이 떨어지지 않았습니다.
+	3초 시점의 3은 1초뒤에 가격이 떨어집니다. 따라서 1초간 가격이 떨어지지 않은 것으로 봅니다.
+	4초 시점의 2은 1초간 가격이 떨어지지 않았습니다.
+	5초 시점의 3은 0초간 가격이 떨어지지 않았습니다.
+	※ 공지 - 2019년 2월 28일 지문이 리뉴얼되었습니다.
+	 */
+	
+	// 내 풀이
 	public static int[] solution(int[] prices) {
         ArrayList<Integer> arr = new ArrayList<>();
         for(int i=0; i<prices.length;i++) {
         	int time = 0;
         	boolean isFallen = false;
         	for(int k=i+1; k<prices.length;k++) {
-        		System.out.println(prices[i]+" vs " + prices[k]);
         		time++;
         		if(prices[i]>prices[k]) {
         			arr.add(time);
         			isFallen = true;
-        			System.out.println(i+"일때 "+time+" 넣었다");
         			break;
         		}
         	}
@@ -25,10 +44,18 @@ public class StockPrice {
         int[] answer = new int[arr.size()];
         for(int i=0; i<answer.length; i++) {
         	answer[i] = arr.get(i);
-        	System.out.println(answer[i]);
         }
         return answer;
     }
+	
+	// 내 풀이 : 큐/스택 이용
+	public static int[] solution2(int[] prices) {
+        int[] answer = {};
+        
+        return answer;
+    }
+	
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
