ceb0cb8ee77f037178576fa3dbb66031c415277d
ChaSoyoung <yuuni.cha@gmail.com>
2020-05-29 18:04:32 +0900

    20-05-28 Add Number124
    programmers Lv2. Number124 solved

---
+++ Algorithm/src/programmers/level02/Number124.java
@@ -0,0 +1,54 @@
+package programmers.level02;
+
+public class Number123 {
+	/* 124 나라의 숫자 (+6)
+	 * https://programmers.co.kr/learn/courses/30/lessons/12899
+	 * 문제 설명
+124 나라가 있습니다. 124 나라에서는 10진법이 아닌 다음과 같은 자신들만의 규칙으로 수를 표현합니다.
+
+124 나라에는 자연수만 존재합니다.
+124 나라에는 모든 수를 표현할 때 1, 2, 4만 사용합니다.
+예를 들어서 124 나라에서 사용하는 숫자는 다음과 같이 변환됩니다.
+
+10진법	124 나라	10진법	124 나라
+1	1	6	14
+2	2	7	21
+3	4	8	22
+4	11	9	24
+5	12	10	41
+자연수 n이 매개변수로 주어질 때, n을 124 나라에서 사용하는 숫자로 바꾼 값을 return 하도록 solution 함수를 완성해 주세요.
+
+제한사항
+n은 500,000,000이하의 자연수 입니다.
+입출력 예
+n	result
+1	1
+2	2
+3	4
+4	11
+	 * 
+	 */
+	public static String solution(int n) {
+		StringBuilder answer = new StringBuilder();
+        while(n>0) { // n = 12; 3;
+        	answer.insert(0, n%3); // answer = 0; answer = 00;
+//        	if(n%3==0) {
+//        		n-=3;
+//        	}
+//        	n /= 3;
+        	n = (n-1)/3; // n = 11/3 = 3; 2/3 = 0;
+        	System.out.println("현재값 : "+answer.toString());
+        }
+
+        return answer.toString().replaceAll("[0]", "4");
+    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("정답 : "+solution(4)); // 4
+		System.out.println();
+		System.out.println("정답 : "+solution(9)); // 111
+
+	}
+
+}
