10def4ef59baf2ac144e0df87c3631f79ff86ac8
ChaSoyoung <yuuni.cha@gmail.com>
2020-06-21 22:29:26 +0900

    20-06-21 Update NumericRepresentation
    programmers lv.2 solved and added other's solution

--- Algorithm/src/programmers/level02/NumericRepresentation.java
+++ Algorithm/src/programmers/level02/NumericRepresentation.java
@@ -1,7 +1,7 @@
 package programmers.level02;
 
 public class NumericRepresentation {
-	/* 숫자의 표현
+	/* 숫자의 표현(+4)
 	 * https://programmers.co.kr/learn/courses/30/lessons/43165
 	 * 	문제 설명
 n개의 음이 아닌 정수가 있습니다. 이 수를 적절히 더하거나 빼서 타겟 넘버를 만들려고 합니다. 예를 들어 [1, 1, 1, 1, 1]로 숫자 3을 만들려면 다음 다섯 방법을 쓸 수 있습니다.
@@ -24,97 +24,62 @@ numbers	target	return
 문제에 나온 예와 같습니다.
 	 */
 
-	public static int solution3(int target) {
-		if(target%2==0) {
-			int midSum = target/2;
-			int start = 0;
-			if(midSum%2==1) {
-				
-			}
-		}
-		
-		
-		
-		return 0;
-	}
-	
-	
-	public static int solution2(int target) {
-        int answer = 0;
-        int start = 1;
-        while(start <= target/2) {
-        	answer += represent2(start, target);
-        	start++;
+	public static int solution(int target) {
+		int answer = 0;
+        int count = 2;
+        while(count <= target/2) {
+        	answer += represent(count, target);
+        	count++;
         }
         return answer+1;
-    }
-	
-	public static int represent2(int start, int target) {
-		int number = target/2;
-		while(number>0) {
-			if(number*(number+2*start-1) == 2*target) {
-				return 1;
-			} else {
-				number--;
-			}
-		}
-		return 0;
 	}
-	public static int solution(int target) {
+	
+	public static int represent(double count, double target) {
+		double startNum = -count/2+0.5+target/count;
+		if(startNum>0 && startNum == (int)startNum) {
+			System.out.println(startNum);
+			return 1;
+		} else {
+			return 0;
+		}
+	}
+	
+	
+	// 다른 사람의 풀이 : 주어진 자연수를 연속된 자연수의 합으로 표현하는 방법의 수는 주어진 수의 홀수 약수의 개수와 같다라는 정수론 정리가 있습니다.
+	public int expressions(int num) {
+	        int answer = 0;
+	        for (int i = 1; i <= num; i += 2) {
+	            if (num % i == 0) {
+	                answer++;
+	            }
+	        }
+	        return answer;
+	}
+	
+	// 다른 사람 풀이 2
+	public int other2(int num) {
         int answer = 0;
-        int start = 1;
-        int sum = 0;
-        for(int i=start; i<=target; i++) {
-        	sum += i;
-        	if(sum==target) {
-        		System.out.println("i = "+i);
-        		answer++;
-        		start++;
-        		sum = 0;
-        		i = start;
-        		System.out.println(start+" "+i);
-        	}
-        }
-        int cnt = target/2;
-        while(cnt>=1) {
-        	
-        }
-        System.out.println("중간 집계 "+answer);
-        
-        /*
-        while(start <= target/2) {
-        	int a = represent(number, start, target);
-        	answer+=a;
-        	start++;
-        	if(a>0) {
-        		number--;
-        	}
-        }
-        */
+
+	    for(int fNum=1; fNum <= num; fNum++){
+	    	int chkNum = 0;
+	        for(int sNum=fNum; sNum <=num; sNum++){
+	         chkNum = chkNum + sNum;
+	
+	         if(chkNum == num){
+	             answer++;
+	             break;
+	         }else if(chkNum > num){
+	             break;
+	         }
+	      }
+	      //chkNum = 0;
+	    }
+
         return answer;
     }
 	
-	public static int represent(int number, int start, int target) {
-		if(number*(number+2*start-1) == 2*target) {
-				return 1;
-		}
-		return 0;
-	}
-	
-	public static int represent(int start, int target) {
-		int number = target/2;
-		while(number>0) {
-			if(number*(number+2*start-1) == 2*target) {
-				return 1;
-			} else {
-				number--;
-			}
-		}
-		return 0;
-	}
-	
 	public static void main(String[] args) {
-		System.out.println(solution2(131));
+		System.out.println("답 : "+solution(18));
 
 	}
 
