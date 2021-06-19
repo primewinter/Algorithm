3d4a09531ae6b3903650c53536c4b85f8968c294
primewinter <yuuni.cha@gmail.com>
2021-06-20 00:03:15 +0900

    update: 정렬 - 소트인사이드 주석 추가

--- Algorithm/src/baekjoon/sort/q1427/Q1427.java
+++ Algorithm/src/baekjoon/sort/q1427/Q1427.java
@@ -5,6 +5,10 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.Scanner;
 
+/**
+ * TITLE: BOJ 소트인사이드
+ * URL: https://www.acmicpc.net/problem/1427
+ */
 public class Q1427 {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
@@ -17,8 +21,6 @@ public class Q1427 {
 
         Arrays.sort(arr, Comparator.reverseOrder());
 
-//        sort(arr, 0, arr.length - 1);
-
         StringBuilder sb = new StringBuilder();
         for(int val : arr) {
             sb.append(val);
@@ -27,45 +29,6 @@ public class Q1427 {
         System.out.println(sb);
     }
 
-    static void sort(int[] arr, int hi, int lo) {
-        
-        if( hi >= lo ) return;
-        
-        int pivot = partition(arr, hi, lo);
-
-        sort(arr, hi, pivot - 1);
-        sort(arr, pivot + 1, lo);
-    }
-
-    static int partition(int[] arr, int left, int right) {
-        
-        int hi = left;
-        int lo = right;
-        int pivot = arr[left];
-
-        while( hi > lo ) {
-
-            while( arr[hi] > pivot && hi > lo ) {
-                lo--;
-            }
-
-            while( arr[lo] <= pivot && lo < hi ) {
-                lo++;
-            }
-
-            swap(arr, lo, hi);
-
-        }
-
-        swap(arr, left, lo);
-
-        return lo;
-    }
-
-    static void swap(int[] arr, int i , int j) {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-    }
+    
 
 }
